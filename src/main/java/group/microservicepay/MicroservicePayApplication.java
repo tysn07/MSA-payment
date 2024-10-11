e05322076c6967366c560a5d9b6eb2c90ed7a726
tysn07 <tyler0702@naver.com>
2024-10-11 20:27:30 +0900

    Initial Commit

---
+++ src/main/java/group/microservicepay/MicroservicePayApplication.java
@@ -0,0 +1,13 @@
+package group.microservicepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group/microservicepay/MicroservicePayApplicationTests.java
@@ -0,0 +1,13 @@
+package group.microservicepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
