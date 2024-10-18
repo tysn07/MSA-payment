29e7bc6fec93cbad03f4f1a127626c1160b8ad60
tysn07 <tyler0702@naver.com>
2024-10-18 13:28:22 +0900

    :recycle: change feign client address

--- src/main/java/group/microservicepay/remote/feign/OrderFeignClient.java
+++ src/main/java/group/microservicepay/remote/feign/OrderFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-@FeignClient(name = "microservice-order", url = "http://localhost:8082/external")
+@FeignClient(name = "microservice-order", url = "http://15.165.174.43:8082/external")
 public interface OrderFeignClient {
 
     @GetMapping("/orders/{orderId}")
